3c4484a03eb2d3bd84d7fd3ad93a19743347c9e8
nwuensche <Wuensche.Niklas@gmail.com>
2021-04-22 12:44:35 +0200

    Use lazy for ShapeRenderer

--- core/src/de/nwuensche/stellenwerttafel/MyGdxGame.kt
+++ core/src/de/nwuensche/stellenwerttafel/MyGdxGame.kt
@@ -8,16 +8,19 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.utils.ScreenUtils
 
 class MyGdxGame : ApplicationAdapter() {
-    lateinit var sR: ShapeRenderer
-    val board: Board by lazy { Board() }
-    override fun create() {
-        Gdx.gl.glLineWidth(10f)
+    val sR: ShapeRenderer by lazy { // Used for drawing lines,circles,...
         val camera = OrthographicCamera(Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
                 .apply { setToOrtho(true, Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat()) } // INFO Resets origin (0,0) to top left
 
-        sR = ShapeRenderer()
+        ShapeRenderer()
                 .apply { color = Color.BLACK }
                 .apply { projectionMatrix = camera.combined }
+    }
+    val board: Board by lazy { Board() }
+
+    override fun create() {
+        Gdx.gl.glLineWidth(10f)
+
     }
 
     override fun render() {
